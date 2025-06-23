f1d983b54a554ae2f33d8add37b6ef1de96afb55
unjoinable <135444615+unjoinable@users.noreply.github.com>
2025-06-23 13:35:02 +0530

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "io.github.unjoinable"
+version = "1.0.0-BETA"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom-snapshots:9604a39476")
+}
---
+++ src/main/java/io/github/unjoinable/Veltrix.java
@@ -0,0 +1,3 @@
+package io.github.unjoinable;
+
+public class Veltrix {}
